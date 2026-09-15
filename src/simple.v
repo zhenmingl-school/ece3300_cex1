f90b88598820abd14150a6cde6162585ba759e66
zhenmingl-school <zhenmingl@cpp.edu>
2026-09-14 17:34:50 -0700

    Fix case statements in simple.v for correct syntax

--- src/simple.v
+++ src/simple.v
@@ -27,12 +27,12 @@ always @(*) begin
 	case (a)
 		3'b000: result = 7'b0000000;
 		3'b001: result = 7'b0000001;
-		3'b010; result = 7'b0000011;
-		3'b011; result = 7'b0000111;
+		3'b010: result = 7'b0000011;
+		3'b011: result = 7'b0000111;
 		3'b100: result = 7'b0001111;
 		3'b101: result = 7'b0011111;
-		3'b110; result = 7'b0111111;
-		3'b111; result = 7'b1111111;
+		3'b110: result = 7'b0111111;
+		3'b111: result = 7'b1111111;
 
 	endcase
 end
